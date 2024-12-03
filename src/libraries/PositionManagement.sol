012430b955571d8aafa03fe06d78b29e4e13bffc
Bhargav Varma <bhargavvarma@calibraint.com>
2024-12-03 14:59:17 +0530

    trail

--- src/libraries/PositionManagement.sol
+++ src/libraries/PositionManagement.sol
@@ -3,40 +3,40 @@ pragma solidity ^0.8.26;
 
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
-import {PositionInfo} from "../libraries/PositionInfoLibrary.sol";
+import {PositionInfoLibrary} from "../libraries/PositionInfoLibrary.sol";
 
 library PositionManagement {
+    using PositionInfoLibrary for uint256;
+    
     error InvalidTokenId();
     error PoolNotFound();
 
     function getPoolAndPositionInfo(
-        mapping(uint256 => PositionInfo) storage positions,
+        mapping(uint256 => uint256) storage positions,
         mapping(bytes25 => PoolKey) storage poolKeys,
         uint256 tokenId
-    ) internal view returns (PoolKey memory poolKey, PositionInfo memory info) {
+    ) internal view returns (PoolKey memory poolKey, uint256 info) {
         info = positions[tokenId];
-        if (info.poolId == bytes25(0)) revert InvalidTokenId();
+        if (info == 0) revert InvalidTokenId();
         
-        poolKey = poolKeys[info.poolId];
+        bytes25 poolId = PositionInfoLibrary.poolId(info);
+        poolKey = poolKeys[poolId];
         if (poolKey.currency0.toId() == 0) revert PoolNotFound();
         
         return (poolKey, info);
     }
 
     function storeNewPosition(
-        mapping(uint256 => PositionInfo) storage positions,
+        mapping(uint256 => uint256) storage positions,
         mapping(bytes25 => PoolKey) storage poolKeys,
         uint256 tokenId,
         PoolKey memory poolKey,
         int24 tickLower,
         int24 tickUpper
     ) internal {
-        bytes25 poolId = poolKey.toId();
+        uint256 info = PositionInfoLibrary.initialize(poolKey, tickLower, tickUpper);
+        bytes25 poolId = PositionInfoLibrary.poolId(info);
         poolKeys[poolId] = poolKey;
-        positions[tokenId] = PositionInfo({
-            poolId: poolId,
-            tickLower: tickLower,
-            tickUpper: tickUpper
-        });
+        positions[tokenId] = info;
     }
 }
